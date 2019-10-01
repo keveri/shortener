8931f4fd0760bbfd163d9121a795c1c599a12dc5
Matias Keveri <keveri@users.noreply.github.com>
2019-10-01 16:34:12 +0300

    Use QuasiQuoter for constructing JSON values
    closes #9

--- test/Spec.hs
+++ test/Spec.hs
@@ -24,18 +24,17 @@ spec = with (return app) $ do
         it "responds with 200" $ do
             get "/short/abc" `shouldRespondWith` 200
         it "responds with ShortUrl" $ do
-            let testData = "{\"originalUrl\":\"original_url\",\"shortenedUrl\":\"short_url\"}"
+            let testData = [json|{originalUrl: "original_url", shortenedUrl: "short_url"}|]
             get "/short/abc" `shouldRespondWith` testData
         it "responds with 404 if the short URL does'n exist" $ do
             get "/short/nonexisting" `shouldRespondWith` 404
     describe "POST /short" $ do
+        let urlData = [json|{url: "orig"}|]
         it "responds with 200" $ do
-            let urlData = "{\"url\":\"https://hoogle.haskell.org/\"}"
             postJson "/short" urlData  `shouldRespondWith` 200
         it "responds with short URL data" $ do
-            let urlData = "{\"url\":\"orig\"}"
-            let shortUrlData = "{\"originalUrl\":\"orig\",\"shortenedUrl\":\"short_url\"}"
+            let shortUrlData = [json|{originalUrl: "orig", shortenedUrl: "short_url"}|]
             postJson "/short" urlData  `shouldRespondWith` shortUrlData
         it "responds with 400 with invalid data" $ do
-            let invalidData = "{\"invalid\":\"will_fail\"}"
+            let invalidData = [json|{invalid: "will_fail"}|]
             postJson "/short" invalidData  `shouldRespondWith` 400
