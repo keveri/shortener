e100c7871c88e7f2a71d87fd660b74f872d57727
Matias Keveri <keveri@users.noreply.github.com>
2019-10-26 19:43:50 +0300

    Add database with 'persistent' library
    Try out persistent library even though it's overkill for this simple
    app. Also add a simple token generation.

--- src/Shortener/Config.hs
+++ src/Shortener/Config.hs
@@ -3,16 +3,37 @@
 module Shortener.Config
     ( readConfig
     , Config(..)
+    , DbConfig(..)
     ) where
 
-import qualified Data.Configurator as C
+import qualified Data.Configurator       as C
+import qualified Data.Configurator.Types as T
 
 data Config = Config
-  { cPort :: Int
+  { cPort     :: Int
+  , cDbConfig :: DbConfig
+  } deriving (Show, Eq)
+
+data DbConfig = DbConfig
+  { dbHost :: String
+  , dbPort :: Int
+  , dbUser :: String
+  , dbPass :: String
+  , dbName :: String
   } deriving (Show, Eq)
 
 readConfig :: FilePath -> IO Config
 readConfig cfgFile = do
     cfg  <- C.load [C.Required cfgFile]
     port <- C.require cfg "port"
-    return $ Config port
+    db   <- parseDB cfg
+    return $ Config port db
+
+parseDB :: T.Config -> IO DbConfig
+parseDB cfg = do
+    host <- C.require cfg "db.host"
+    port <- C.require cfg "db.port"
+    user <- C.require cfg "db.user"
+    pass <- C.require cfg "db.pass"
+    name <- C.require cfg "db.name"
+    return $ DbConfig host port user pass name
--- src/Shortener/Lib.hs
+++ src/Shortener/Lib.hs
@@ -1,34 +1,58 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Shortener.Lib
     ( startApp
     , app
+    , Env(..)
     ) where
 
-import Network.Wai.Handler.Warp
-import Servant
+import           Control.Monad.IO.Class      (liftIO)
+import           Data.Int                    (Int64)
+import           Data.Text
+import           Database.Persist.Postgresql (ConnectionString)
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger          (withStdoutLogger)
+import           Servant
+import           Text.StringRandom
 
-import Shortener.Api
-import Shortener.Config
+import           Database.Persist            (Entity)
+import           Shortener.Api
+import           Shortener.Config
+import           Shortener.Database
+import           Shortener.Schema
 
-startApp :: Config -> IO ()
-startApp config = do
-  let port = cPort config
-  run port app
+newtype Env = Env
+  { envConnectionString :: ConnectionString
+  }
 
-app :: Application
-app = serve api server
+startApp :: Config -> Env -> IO ()
+startApp config env = withStdoutLogger $ \aplogger -> do
+        let port = cPort config
+            settings = setPort port $ setLogger aplogger defaultSettings
+        runSettings settings (app env)
 
-server :: Server API
-server = shortUrlById :<|> createShortUrl
+app :: Env -> Application
+app = serve api . server
 
-shortUrlById :: Id -> Handler ShortUrl
-shortUrlById = \ case
-  "abc" -> return exampleShortUrl
-  _     -> throwError err404
+server :: Env -> Server API
+server env =
+  let connStr = envConnectionString env
+  in shortUrlByToken connStr :<|> createShortUrl connStr
 
-createShortUrl :: OriginalUrl -> Handler ShortUrl
-createShortUrl (OriginalUrl url) = return $ ShortUrl url "short_url"
+randomToken :: IO Text
+randomToken = stringRandomIO "[a-zA-Z]{6}"
 
-exampleShortUrl :: ShortUrl
-exampleShortUrl = ShortUrl "original_url" "short_url"
+shortUrlByToken :: ConnectionString -> Text -> Handler ShortUrl
+shortUrlByToken connStr token = do
+  result <- liftIO $ findUrlPG connStr token
+  case result of
+    Just r  -> return r
+    Nothing -> throwError err404
+
+createShortUrl :: ConnectionString -> OriginalUrl -> Handler ShortUrl
+createShortUrl connStr ou@(OriginalUrl url) = do
+  token <- liftIO randomToken
+  mShortUrl <- liftIO $ createShortUrlPG connStr $ ShortUrl url token
+  case mShortUrl of
+      Nothing -> createShortUrl connStr ou
+      Just r  -> return r
